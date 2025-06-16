d5612e9989bf289c1e1144d7df0a3c0d84916a77
Rik Brandenburg <93656269+PS205590@users.noreply.github.com>
2025-06-16 13:34:44 +0200

    Feat(Install API and return Customers)

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\CustomerController;
+use App\Http\Controllers\Api\V1\InvoiceController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('customers', CustomerController::class);
+    Route::apiResource('invoices', InvoiceController::class);
+
+    Route::get('/hello', function () {
+        return 'simple test';
+    });
+});
+
