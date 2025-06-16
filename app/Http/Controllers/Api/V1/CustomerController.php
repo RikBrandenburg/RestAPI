61a5fbe6acfd2da605575b4943011319fb975d8f
Rik Brandenburg <93656269+PS205590@users.noreply.github.com>
2025-06-16 16:43:38 +0200

    Feat(PUT or PATCH possible in Customer)

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -66,7 +66,7 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
     }
 
     /**
