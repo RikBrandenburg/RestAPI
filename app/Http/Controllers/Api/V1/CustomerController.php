bf448d353b3a800fdc0cf76dbd6fc466852138e5
Rik Brandenburg <93656269+PS205590@users.noreply.github.com>
2025-06-16 14:06:26 +0200

    Feat(Added a CustomerQuery option inside the Services)

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
+use Illuminate\Http\Request;
 use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
+use App\Services\V1\CustomerQuery;
 
 
 class CustomerController extends Controller
@@ -17,8 +19,17 @@ class CustomerController extends Controller
      * 
      * @return \Illuminate\Http\Resources\Json\CustomerCollection
      */
-    public function index()
+    public function index(Request $request)
     {
+        $filter = new CustomerQuery();
+        $queryItems = $filter->transform($request);
+
+        if (count($queryItems) === 0) {
+            return new CustomerCollection(Customer::paginate());
+        } else {
+            return new CustomerCollection(Customer::where($queryItems)->paginate());
+        }
+
         return new CustomerCollection(Customer::paginate());
     }
 
